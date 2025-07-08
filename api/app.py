d50b74c9ea39e390dde17cafd4a11faaa45f7853
Andres F Velasco <afvelasco@gmail.com>
2025-07-07 20:42:52 -0500

    Se completa la Api rest y se incluye programa consumidor

--- api/app.py
+++ api/app.py
@@ -23,22 +23,52 @@ class EspecialistaLista(Resource):
     
     def post(self):
         nuevo_esp = request.json
-        print(nuevo_esp)
-        sql = f"INSERT INTO especialistas (id_especialista,nombre,especialidad,foto) VALUES ('{nuevo_esp["id"]}','{nuevo_esp["nombre"]}','{nuevo_esp["especialidad"]}','{nuevo_esp["foto"]}')"
+        sql = f"SELECT * FROM especialistas WHERE id_especialista='{nuevo_esp["id"]}'"
         mi_cursor.execute(sql)
-        mi_DB.commit()
-        return jsonify({"mensaje" : "Especialista Agregado"})
-"""
-class ProductoItem(Resource):
-    def get(self,id):
-        for item in productos:
-            if item['id']==id:
-                return jsonify(item)
-        return jsonify({"mensaje":"Producto no encontrado"})
-    """
+        resultado = mi_cursor.fetchall()
+        if len(resultado)==0:
+            sql = f"INSERT INTO especialistas (id_especialista,nombre,especialidad,foto) VALUES ('{nuevo_esp["id"]}','{nuevo_esp["nombre"]}','{nuevo_esp["especialidad"]}','{nuevo_esp["foto"]}')"
+            mi_cursor.execute(sql)
+            mi_DB.commit()
+            return jsonify({"mensaje" : "Especialista Agregado"})
+        else:
+            return jsonify({"mensaje" : "Id de Especialista ya existe"})
+
+class Especialista(Resource):
+    def get(self, id):
+        sql = f"SELECT * FROM especialistas WHERE id_especialista='{id}'"
+        mi_cursor.execute(sql)
+        esp = mi_cursor.fetchall()
+        if len(esp)>0:
+            esp = esp[0]
+            return jsonify({"id":esp[0], "nombre":esp[1], "especialidad":esp[2], "foto": esp[3]})
+        else:
+            return jsonify({"mensaje":"Especialista no encontrado"})
+        
+    def put(self, id):
+        esp = request.json
+        sql = f"SELECT * FROM especialistas WHERE id_especialista='{id}'"
+        mi_cursor.execute(sql)
+        esp = mi_cursor.fetchall()
+        if len(esp)>0:
+            sql = f"UPDATE especialistas SET nombre='{esp[1]}', especialidad='{esp[2]}', foto='{esp[3]}' WHERE id_especialista='{esp[0]}'"
+            return jsonify({"mensaje":"Especalista actualizado"})
+        else:
+            return jsonify({"mensaje":"Especialista no encontrado"})
+    
+    def delete(self, id):
+        sql = f"SELECT * FROM especialistas WHERE id_especialista='{id}'"
+        mi_cursor.execute(sql)
+        esp = mi_cursor.fetchall()
+        if len(esp)>0:
+            sql = f"DELETE FROM especialistas WHERE id_especialista='{id}'"
+            return jsonify({"mensaje":"Especalista eliminado"})
+        else:
+            return jsonify({"mensaje":"Especialista no encontrado"})
+
         
 api.add_resource(EspecialistaLista,"/especialistas")
-#api.add_resource(ProductoItem,"/productos/<int:id>")
+api.add_resource(Especialista,"/especialistas/<int:id>")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True,port=8001)
--- api/consumidor.py
+++ api/consumidor.py
@@ -3,7 +3,18 @@ import requests
 id = input("Id: ")
 nombre = input("Nombre: ")
 especialidad = input("Especialidad: ")
-
+opcion = input("(A)gregar/(M)odificar/(B)orrar/(L)istado/(U)no: ")
 nuevo_esp = {"id":id, "nombre":nombre, "especialidad": especialidad, "foto": ""}
-respuesta = requests.post("http://10.6.125.199:8001/especialistas", json = nuevo_esp)
-print(respuesta)
+if opcion =="A":
+    respuesta = requests.post(f"http://10.6.126.39:8001/especialistas", json = nuevo_esp)
+elif opcion == "M":
+    respuesta = requests.put(f"http://10.6.126.39:8001/especialistas/{id}", json = nuevo_esp)
+elif opcion == "B":
+    respuesta = requests.delete(f"http://10.6.126.39:8001/especialistas/{id}")
+elif opcion == "L":
+    respuesta = requests.get(f"http://10.6.126.39:8001/especialistas")
+elif opcion == "U":
+    respuesta = requests.get(f"http://10.6.126.39:8001/especialistas/{id}")
+else:
+    respuesta = {"mensaje":"Opción no disponible"}
+print(respuesta.json())
