3d542a28b46e3e6804bd076f09a2223d0be183b0
Andres F Velasco <afvelasco@gmail.com>
2025-10-14 20:49:01 -0500

    Se actualiza consumo microservicios desde java script (frontend)

--- models/usuarios.py
+++ models/usuarios.py
@@ -4,10 +4,9 @@ class Usuario:
     def loguear(self, id, contra):
         cifrada = hashlib.sha512(contra.encode("utf-8")).hexdigest()
         sql = f"SELECT nombre FROM usuarios WHERE id='{id}' AND contra='{cifrada}'"
+        print(sql)
         mi_cursor.execute(sql)
         resultado=mi_cursor.fetchall()
         return resultado
 
-
 mi_usuarios = Usuario()
-        
